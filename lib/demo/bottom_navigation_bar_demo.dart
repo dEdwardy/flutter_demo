ce9be78569859177f251d4b2301d647c2fd4ee82
Edward <957196958@qq.com>
2020-01-15 10:01:47 +0800

    3

---
+++ lib/demo/bottom_navigation_bar_demo.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationBarDemo extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _BottomNavigationBarState();
+  }
+}
+class _BottomNavigationBarState extends State<BottomNavigationBarDemo> {
+  int _currentIndex = 2;
+  void _onTapHandler(int index) {
+    setState((){
+      _currentIndex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          fixedColor: Colors.black,
+          currentIndex: _currentIndex,
+          onTap: _onTapHandler,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.explore),
+              title:Text('Explore'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history),
+              title:Text('History'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              title:Text('List'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title:Text('My'),
+            ),
+          ],
+        );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './demo/listview_demo.dart';
 import './demo/drawer_demo.dart';
+import './demo/bottom_navigation_bar_demo.dart';
 
 void main() => runApp(new App());
 
@@ -54,12 +55,14 @@ class Home extends StatelessWidget {
         // body: ListViewDemo()),
         body: TabBarView(
           children: <Widget>[
-            Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+            ListViewDemo(),
+            // Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
             Icon(Icons.change_history, size: 128.0, color: Colors.black12),
             Icon(Icons.directions_bike, size: 128.0, color: Colors.black12)
           ],
         ),
-        drawer: DrawerDemo()
+        drawer: DrawerDemo(),
+        bottomNavigationBar: BottomNavigationBarDemo(),
       ),
     );
   }
